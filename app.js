ed304a46029041a64808f9e87d4ef63524c16c88
Priyanshu-Prime <pmedu718@gmail.com>
2024-06-28 23:07:25 +0530

    time is functional on the slider

---
+++ app.js
@@ -0,0 +1,16 @@
+let slide = document.getElementById("range");
+
+let time = document.getElementById("curr-time");
+
+
+slide.addEventListener("input", function()
+{
+    let val = Math.floor((213/100)*slide.value);
+    let sec = val%60;
+    if (sec < 10)
+        sec = '0' + sec;
+    let min = Math.floor((val/60));
+    if (min < 10)
+        min = '0' + min;
+    time.innerText = `${min}:${sec}`;
+});
